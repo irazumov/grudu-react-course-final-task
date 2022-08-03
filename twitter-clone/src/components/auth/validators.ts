4be4fc55a0f884c1f9d839de4fee01423f502bed
Ivan Razumov <ivan.razumov@macys.com>
2022-08-03 12:09:12 +0400

    feat(validators): created input validators

---
+++ twitter-clone/src/components/auth/validators.ts
@@ -0,0 +1,32 @@
+import { ValidationFunc } from "../../hooks/validationable";
+import validateEmail from "../../validators/email";
+
+export const email: Array<ValidationFunc<string>> = [
+  (value) => value.length > 0 || "Email is required",
+  (value) => validateEmail(value) || "Email is invalid",
+];
+
+export const password: Array<ValidationFunc<string>> = [
+  (value) => value.length > 0 || "Password is required",
+  (value) => value.length >= 8 || "Password must be at least 8 characters",
+  (value) => value.length <= 256 || "Password must be at most 256 characters",
+];
+
+export const username: Array<ValidationFunc<string>> = [
+  (value) => value.length > 0 || "Username is required",
+  (value) => value.length >= 1 || "Username must be at least 1 characters",
+  (value) => value.length <= 256 || "Username must be at most 256 characters",
+];
+
+export const fullname: Array<ValidationFunc<string>> = [
+  (value) => value.length > 0 || "Fullname is required",
+  (value) => value.length >= 1 || "Fullname must be at least 1 characters",
+  (value) => value.length <= 256 || "Fullname must be at most 256 characters",
+];
+
+export default {
+  email,
+  password,
+  username,
+  fullname,
+};
