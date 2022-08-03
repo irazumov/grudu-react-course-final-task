8bea03a7c055debc4251127826d032b2ea20cb98
Ivan Razumov <ivan.razumov@macys.com>
2022-08-03 12:06:40 +0400

    feat(validators): email validator

---
+++ twitter-clone/src/validators/email.ts
@@ -0,0 +1,12 @@
+export default function validateEmail(email: string): boolean {
+  if (!email) {
+    return false;
+  }
+  if (typeof email !== 'string') {
+    return false;
+  }
+  if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+    return false;
+  }
+  return true;
+}
