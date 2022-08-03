974d3f58c51b1dd49538fdc927c299f51785e20f
Ivan Razumov <ivan.razumov@macys.com>
2022-08-03 21:29:15 +0400

    feat(api): filters

--- twitter-clone/src/api/index.ts
+++ twitter-clone/src/api/index.ts
@@ -1,8 +1,14 @@
 export class ApiClient<T> {
   constructor(private readonly api: string) {}
 
-  public find(): Promise<T[]> {
-    return fetch(this.api).then(res => res.json()) as Promise<T[]>;
+  public find(filters?: Partial<T>): Promise<T[]> {
+    const search = new URLSearchParams();
+    Object.entries(filters || {}).forEach(([key, value]) => {
+      search.append(key, `${value}`);
+    });
+    const query = search.toString();
+    const url = `${this.api}${query && '?' + query}`;
+    return fetch(url).then(res => res.json()) as Promise<T[]>;
   }
 
   public findById(id: string): Promise<T> {
