9af5a5b58b8cd11de58aa363f2d085b6fb776c71
Ivan Razumov <ivan.razumov@macys.com>
2022-08-03 12:06:11 +0400

    feat(components): form wrapper component

---
+++ twitter-clone/src/components/auth/FormWrapper.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./FormWrapper.css";
+import TButton from '../ui/TButton';
+
+interface ISubrow {
+  text: string;
+  linkTitle: string;
+  linkHref: string;
+}
+
+interface FormWrapperProps {
+  children: React.ReactNode;
+  className?: string;
+  label: string;
+  submitBtnText: string;
+  subrow: ISubrow;
+  onSubmit?: (event: React.FormEvent<HTMLFormElement>) => void;
+}
+
+export default function FormWrapper(props: FormWrapperProps) {
+  const {
+    children,
+    className,
+    onSubmit,
+    subrow,
+    submitBtnText,
+  } = props;
+
+  return (
+    <>
+      <form
+        className={`form ${className}`}
+        onSubmit={onSubmit}
+        aria-label={props.label}
+      >
+        <label>{submitBtnText}</label>
+        <div className="main">{children}</div>
+        <div className="footer">
+          <TButton className="bordered" type="submit">{submitBtnText}</TButton>
+        </div>
+      </form>
+      <p
+        className="subrow">{subrow.text} <a href={subrow.linkHref}>{subrow.linkTitle}</a>
+      </p>
+    </>
+  );
+}
