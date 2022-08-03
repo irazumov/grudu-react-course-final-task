9ee09c4d8d8d39843b28a107f0faf30fa1a37c9a
Ivan Razumov <ivan.razumov@macys.com>
2022-08-03 21:12:31 +0400

    feat(tweet):  basic tweets adding

---
+++ twitter-clone/src/api/users.ts
@@ -0,0 +1,11 @@
+import { ApiClient } from ".";
+
+export interface IUser {
+  id: string;
+  name: string;
+  email: string;
+}
+
+const usersApi = new ApiClient<IUser>("http://localhost:3001/users");
+
+export default usersApi;
