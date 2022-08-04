e3df1f846229f07a802c5c5b02608d9ecb8c382f
Ivan Razumov <ivan.razumov@macys.com>
2022-08-04 10:41:11 +0400

    fix: lint fixes

--- twitter-clone/src/components/main/MainContent.tsx
+++ twitter-clone/src/components/main/MainContent.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import './MainContent.css';
 import TweetForm from '../tweets/TweetForm';
 import TweetsList from '../tweets/TweetsList';
