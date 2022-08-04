38c0dcad55e6aab94d9bb77e098c84d763dc60f1
Ivan Razumov <ivan.razumov@macys.com>
2022-08-04 09:33:51 +0400

    feat(header): show username abbr

--- twitter-clone/src/components/main/MainHeader.tsx
+++ twitter-clone/src/components/main/MainHeader.tsx
@@ -1,7 +1,10 @@
+import { useAuth } from '../../contexts/auth';
 import AppLogo from '../icons/AppLogo';
 import './MainHeader.css';
 
 export default function MainHeader() {
+  const auth = useAuth();
+  const abbr = auth.user?.name?.split(' ').map(i => i[0].toUpperCase()).join('') || 'N/A';
   return (
     <header className="main-header">
       <div className="main-header__row">
@@ -10,9 +13,9 @@ export default function MainHeader() {
           <span>Another Twitter Clone</span>
         </div>
         <div className="header-user">
-          <span>John Smith</span>
+          <span>{auth.user?.name}</span>
           <div className="header-user__icon">
-            JS
+            {abbr}
           </div>
         </div>
       </div>
